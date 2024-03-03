a12387dbe7eb569e2d7e2cf1945b34e190d06d1b
hotaru <36354847+hotaru51@users.noreply.github.com>
2024-03-03 14:01:48 +0000

    feat(config): 実行ファイルが存在するディレクトリの取得処理を追加

---
+++ config/yaml.go
@@ -0,0 +1,19 @@
+package config
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func getExecutableDirectoryPath() string {
+	executablePath, err := os.Executable()
+	if err != nil {
+		log.Fatalf("cannot get executable path: %s\n", err)
+	}
+
+	executableDir := filepath.Dir(executablePath)
+	log.Printf("executableDir: %s\n", executableDir)
+
+	return executableDir
+}
