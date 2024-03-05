68d7d1f94f32b879f92e9bc5616d1e87210a1473
hotaru <36354847+hotaru51@users.noreply.github.com>
2024-03-05 04:00:04 +0000

    feat(config): 読み込むconfig.yamlを選択する関数を追加

--- config/config.go
+++ config/config.go
@@ -8,4 +8,6 @@ type Config struct {
 
 const (
 	API_ENDPOINT = "https://api.gandi.net/v5/livedns/domains"
+	CONFIG_FILE_PATH = "/etc/htrap/dns-record-updater"
+	CONFIG_FILE_NAME = "config.yaml"
 )
--- config/yaml.go
+++ config/yaml.go
@@ -17,8 +17,23 @@ func getExecutableDirectoryPath() string {
 
 	return executableDir
 }
+
 func isConfigFileExists(filePath string) bool {
 	_, err := os.Stat(filePath)
 
 	return err == nil
+}
+
+func selectConfigFilePath() string {
+	localConfigPath := getExecutableDirectoryPath() + "/" + CONFIG_FILE_NAME
+	if isConfigFileExists(localConfigPath) {
+		return localConfigPath
+	}
+
+	globalConfigPath := CONFIG_FILE_PATH + "/" + CONFIG_FILE_PATH
+	if isConfigFileExists(globalConfigPath) {
+		return globalConfigPath
+	}
+
+	return ""
 }
