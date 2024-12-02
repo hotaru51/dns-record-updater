0505d4f03765427ef8a6f6e84d800b281c889e10
hotaru <36354847+hotaru51@users.noreply.github.com>
2024-12-02 15:56:06 +0000

    feat: 動作確認でGetMyIP()呼び出し

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"log"
 
 	"github.com/hotaru51/dns-record-updater/config"
+	"github.com/hotaru51/dns-record-updater/myip"
 )
 
 func main() {
 	log.Println("load config file")
 	config := config.LoadConfig()
 	log.Println(config)
+	log.Println(myip.GetMyIP())
 }
