7b5302ff2a4df47ea1d541f7d7491d7b710f21a0
hotaru <36354847+hotaru51@users.noreply.github.com>
2024-12-08 03:18:20 +0000

    feat(gandi): RecordのString()を実装

--- gandi/records.go
+++ gandi/records.go
@@ -1,6 +1,10 @@
 // Gnadi APIを操作するパッケージ
 package gandi
 
+import (
+	"fmt"
+)
+
 /*
 DNSレコード
 */
@@ -10,4 +14,17 @@ type Record struct {
 	RrsetType   string   `json:"rrset_type"`   // レコードタイプ
 	RrsetValues []string `json:"rrset_values"` // レコードの値
 	RrsetHref   string   `json:"rrset_href"`   // レコードのURL
+}
+
+/*
+Recordを文字列で返す
+*/
+func (r *Record) String() string {
+	return fmt.Sprintf(
+		"name: %s, TTL: %d, type: %s, value: %v",
+		r.RrsetName,
+		r.RrsetTTL,
+		r.RrsetType,
+		r.RrsetValues,
+	)
 }
